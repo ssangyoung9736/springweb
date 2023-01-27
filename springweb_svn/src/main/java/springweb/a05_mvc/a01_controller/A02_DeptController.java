44a5338bcf749cc84a928f3c4fdec98e6bdeb7b9
user <user@DESKTOP-0L11UB0>
2023-01-27 14:38:43 +0900

    111

--- springweb_svn/src/main/java/springweb/a05_mvc/a01_controller/A02_DeptController.java
+++ springweb_svn/src/main/java/springweb/a05_mvc/a01_controller/A02_DeptController.java
@@ -3,6 +3,7 @@ package springweb.a05_mvc.a01_controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springweb.a05_mvc.a02_service.A02_DeptService;
@@ -15,7 +16,7 @@ public class A02_DeptController {
 	private A02_DeptService service;
 	
 	@RequestMapping("myDeptList.do")
-	public String myDeptList(Dept sch, Model d) {
+	public String myDeptList(@ModelAttribute("sch") Dept sch, Model d) {
 		d.addAttribute("dlist", service.getDeptList(sch));
 		return "WEB-INF\\views\\a05_mvc\\a02_deptList.jsp";
 	}
