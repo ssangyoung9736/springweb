f5fcd0f644898a29b1064c25f676a020cd792971
ssangyoung9736 <zelra@DESKTOP-H2TRC28>
2023-02-16 15:19:50 +0900

    111

---
+++ springweb_svn/src/main/java/springweb/a05_mvc/a03_dao/A20_CalendarDao.java
@@ -0,0 +1,12 @@
+package springweb.a05_mvc.a03_dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import springweb.z01_vo.Calendar;
+
+@Repository
+public interface A20_CalendarDao {
+	public List<Calendar> calList();
+}
